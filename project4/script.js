73a00998d9b21935650e8b9a29c19466003e700c
AMarrero00 <143424881+AMarrero00@users.noreply.github.com>
2023-10-29 18:55:02 -0400

    Add files via upload

---
+++ project4/script.js
@@ -0,0 +1,31 @@
+const API_KEY = "8c3c1dccb9695938d51be8321771d1c6"; // Reemplaza esto con tu API key
+
+    $(document).ready(function () {
+        $("#checkWeather").click(function () {
+            let city = $("#location").val();
+
+            $.ajax({
+                url:`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                let forecastHtml = "<h5>Pronóstico de 7 días:</h5>";
+
+                data.list.forEach((day) => {
+                        forecastHtml += `<strong>Fecha:</strong> ${new Date(day.dt*1000).toDateString()} ${new Date(day.dt * 1000).toTimeString()}<br>
+                            <strong>Temperatura:</strong>${day.main.temp}°F <br>
+                            <img src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png">
+                            <strong>Descripción:</strong> ${day.weather[0].description} <br>
+                            <hr>
+                        `;
+                    });
+
+                    $("#weatherResult").html(forecastHtml);
+                },
+
+                    error: function () {
+                        $("#weatherResult").html("<p>Error al obtener el clima. Por favor, inténtalo de nuevo.</p>");
+            },
+        });
+    });
+});
